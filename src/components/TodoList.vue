673d168be48ef43fb0c6e2e7e2c412784cbe473e
AhmadGDev <Ahmad.ghelem@gmail.com>
2019-10-07 16:24:31 +0200

    Link component TodoInputText.vue to TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,7 +3,14 @@
 </template>
 
 <script>
+
+import TodoInputText from '@/components/TodoInputText.vue';
+
 export default {
+    name: "app",
+    components: {
+        TodoInputText
+    }
 
 }
 </script>

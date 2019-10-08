0af49a1337c2909192a3fc655d13109929818f19
AhmadGDev <Ahmad.ghelem@gmail.com>
2019-10-08 09:03:27 +0200

    Link v-model in TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,24 @@
 <template>
-  <TodoInputText />
+<div>
+  <TodoInputText v-model="newTodoText" />
+  <TodoListItem :todos="todos" />
+</div>
 </template>
 
 <script>
 
 import TodoInputText from '@/components/TodoInputText.vue';
+import TodoListItem from '@/components/TodoListItem.vue';
 
 export default {
     name: "app",
     components: {
-        TodoInputText
+        TodoInputText,
+        TodoListItem
     },
     data() {
         return {
+            newTodoText: "",
             todos: ["Tomates", "Lait", "Gâteaux"]
         };
     }

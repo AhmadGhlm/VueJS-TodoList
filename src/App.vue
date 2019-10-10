82fa652583857d3ee78d133172f647f0cc57f128
AhmadGDev <Ahmad.ghelem@gmail.com>
2019-10-10 11:04:34 +0200

    Change color text and create an border at TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
-    <div id="app" class="container">
-        <h1 class="title is-1">My Todo List</h1>
-        <TodoList />
-    </div>
+  <div id="app" class="container">
+    <h1 class="title is-1 has-text-white">My Todo List</h1>
+    <TodoList />
+  </div>
 </template>
 
 <script>
-
-import TodoList from '@/components/TodoList.vue';
+import TodoList from "@/components/TodoList.vue";
 
 export default {
-    name: "app",
-    components: {
-        TodoList
-    }
+  name: "app",
+  components: {
+    TodoList
+  }
 };
 </script>
 
 <style lang="scss">
-.container{
-    margin: 100px 60px 0 100px;
+.container {
+  margin: 100px 80px 0 100px;
+  background-color: #131313;
+  border-radius: 10px;
+  color: #fff;
+}
+
+h1 {
+  text-align: center;
 }
 </style>
